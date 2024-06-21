e8caf7220d1c541ff8ce5ba047b52aa67f065f67
Punya Modi <68418104+punyamodi@users.noreply.github.com>
2024-06-22 02:51:23 +0530

    Add files via upload
    Updated

--- Catalouge_Scorerv4.py
+++ Catalouge_Scorerv4.py
@@ -3,26 +3,15 @@ import streamlit as st
 import PyPDF2
 import os
 from langchain_community.llms import HuggingFaceHub
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+
 
 #Please replace your Hugging face access token in the below 'api_key'
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "api_key"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 llm = HuggingFaceHub(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.2", model_kwargs={"temperature": 0.5, "max_length": 64}
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2", model_kwargs={"temperature": 0.5, "max_new_tokens": 10000}
 )
-
-template = """
-        Your task is to meticulously evaluate the text extracted from a catalogue PDF, assigning a score out of 100. Thoroughly analyze the extracted text, identifying any mistakes such as formatting errors, semantic inaccuracies, and inconsistencies. Provide detailed insights into these errors, highlighting their impact on the accuracy and quality of the extracted content. Additionally, offer actionable suggestions for improvements aimed at enhancing the extraction process. Focus on optimizing precision, ensuring semantic fidelity, and achieving a coherent presentation of information. Your evaluation and recommendations are pivotal in refining the extraction process, ultimately contributing to the reliability and usability of the extracted text. Your attention to detail and comprehensive analysis will play a crucial role in improving the overall quality of extracted content from catalogue PDFs.
-        
-        Question: {question}
-        """
-
-prompt = PromptTemplate.from_template(template)
-
-llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 def model(question):
     template = """
@@ -31,12 +20,12 @@ def model(question):
             Question: {question}
             """
 
-    prompt = PromptTemplate.from_template(template)
-
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
-
     st.subheader("Evaluation Result")
-    st.write(llm_chain.run(question))
+    response=llm(template)
+    response_index = response.find("Question: {question}")
+    if response_index != -1:
+        response=response[response_index+20:]
+    st.write(llm(response))
 
 
 def displayPDF(file):
